3091ae700aa1bd57f1aad2f778f8790f1ce67308
Fernando Vargas <fvargas320@gmail.com>
2023-03-30 14:52:58 -0700

    Added rotate and drag and drop logic

--- Battle Boats/Assets/RotateButtonManager.cs
+++ Battle Boats/Assets/RotateButtonManager.cs
@@ -1,14 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RotateButtonManager : MonoBehaviour
 {
-    public new GameObject gameObject;
+    public GameObject gameObjectToRotate;
+    public Button button;
 
-    public void RotateObject()
+    void Start()
     {
-        gameObject.transform.Rotate(new Vector3(0, 0, 90));
+        // Subscribe the OnClick method to the button's click event
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        // Rotate the object
+        gameObjectToRotate.transform.Rotate(new Vector3(0, 0, 90));
+
+        // Highlight the button
+        button.image.color = Color.yellow;
+
+        // Invoke the Unhighlight method after 0.5 seconds
+        Invoke("Unhighlight", 0.5f);
+    }
+
+    void Unhighlight()
+    {
+        // Unhighlight the button
+        button.image.color = Color.white;
+    }
+    void RotateObject()
+    {
+        gameObjectToRotate.transform.Rotate(new Vector3(0, 0, 90));
+    }
 
     }
-}
