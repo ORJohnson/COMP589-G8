11d186822f4f9a908c8864cec2223c8c4902b75a
OladiranJ <oladiran.johnson.950@my.csun.edu>
2023-04-13 12:46:27 -0700

    more tile click stuff

--- Battle Boats/Assets/Scripts/Node.cs
+++ Battle Boats/Assets/Scripts/Node.cs
@@ -15,6 +15,8 @@ public class Node : MonoBehaviour
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        hitColor[0] = gameObject.GetComponent<MeshRenderer>().material.color;
+        hitColor[1] = gameObject.GetComponent<MeshRenderer>().material.color;
     }
 
     // Update is called once per frame
@@ -32,5 +34,28 @@ public class Node : MonoBehaviour
                 }
             }
         }
+    }
+
+    // In case we want to actually have an animation of the missile hitting the tiles
+    /*private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Missile"))
+        {
+            missleHit = true;
+        } else if (collision.gameObject.CompareTag("EnemyMissile"))
+        {
+            hitColor[0] = new Color32(38, 57, 76, 255);
+            GetComponent<Renderer>().material.color = hitColor[0];
+        }
+    }*/
+
+    public void SetTileColor(int index, Color32 color)
+    {
+        hitColor[index] = color;
+    }
+
+    public void SwitchColors(int colorIndex)
+    {
+        GetComponent<Renderer>().material.color = hitColor[colorIndex];
     }
 }
